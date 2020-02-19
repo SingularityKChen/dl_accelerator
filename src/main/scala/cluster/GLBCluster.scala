888a8a666766518961bcf08074bfc4483f392532
SingularityKChen <chency_singularity@163.com>
2020-02-19 23:25:08 +0800

    Finish the logic of PSumSRAMBank; Finish the draft test of PSumSRAmBank; Finish the draft logic of InActSRAMBank;

--- src/main/scala/cluster/GLBCluster.scala
+++ src/main/scala/cluster/GLBCluster.scala
@@ -5,9 +5,9 @@ import chisel3.util._
 import dla.pe.{CSCStreamIO, MCRENFConfig, StreamBitsIO}
 
 class GLBCluster(debug: Boolean) extends Module with ClusterSRAMConfig {
-  val io = new GLBClusterIO
-  private val iSRAMs: Vec[InActSRAMBankIO] = Vec(inActSRAMNum, Module(new InActSRAMBank).io)
-  private val pSRAMs: Vec[PSumSRAMBankIO] = Vec(pSumSRAMNum, Module(new PSumSRAMBank).io)
+  val io: GLBClusterIO = IO(new GLBClusterIO)
+  private val iSRAMs: Vec[InActSRAMBankIO] = Vec(inActSRAMNum, Module(new InActSRAMBank(debug: Boolean)).io)
+  private val pSRAMs: Vec[PSumSRAMBankIO] = Vec(pSumSRAMNum, Module(new PSumSRAMBank(pSumSRAMSize, psDataWidth, debug)).io)
   // connections of data path
   io.dataPath.weightIO.foreach(x => x.inIOs <> x.outIOs)
   io.dataPath.inActIO.zip(iSRAMs).foreach({ case (topIO, sramIO) => topIO <> sramIO.dataPath})
@@ -15,123 +15,160 @@ class GLBCluster(debug: Boolean) extends Module with ClusterSRAMConfig {
   // connections of control path
 }
 
-class PSumSRAMBank extends Module with ClusterSRAMConfig with MCRENFConfig with GNMFCS2Config {
+class PSumSRAMBank(private val theSRAMSize: Int, private val theDataWidth: Int, debug: Boolean) extends SRAMCommon(theSRAMSize, theDataWidth) with ClusterSRAMConfig with MCRENFConfig with GNMFCS2Config {
   private val oneSPadPSum: Int = M0*E*N0*F0 // when read counts this, then stop
   private val oneSRAMPSum: Int = oneSPadPSum * N2*M2*F2 // when write counts this, then finished
   require(oneSRAMPSum <= pSumSRAMSize, s"the size of PSum SRAM is $pSumSRAMSize, " +
     s"which should be larger than the config's requirement, which is $oneSRAMPSum")
-  val io = new PSumSRAMBankIO
-  private val dataSRAM: SyncReadMem[UInt] = SyncReadMem(pSumSRAMSize, UInt(psDataWidth.W))
+  val io: PSumSRAMBankIO = IO(new PSumSRAMBankIO)
   // SRAM read write logic
-  // because it will not read and write at the same time
-  // so we just need one pair of regs to record process
-  private val writeOrRead = RegInit(false.B) // true then "do" means write, false then "do" means read
-  private val startIdx = RegInit(0.U(log2Ceil(pSumSRAMSize).W)) // the first do address, from io
-  private val doIdxReg: UInt = RegInit(0.U(log2Ceil(pSumSRAMSize).W)) // connect to index
-  private val doEnReg = RegInit(false.B)
-  private val countIncWire = Wire(Bool())
+  private val startIdx = Wire(UInt(log2Ceil(pSumSRAMSize).W)) // the first do address, from io
+  private val doIdxWire: UInt = Wire(UInt(log2Ceil(theSRAMSize).W))
   // read or write one SPad size partial sums
   // doIdxCount means current number of PSums that has been done
   // when finish, doDone = true.B
-  private val (doIdxCount, doDone) = Counter(countIncWire, oneSPadPSum + 1)
-  private val doWriteWire = Wire(Bool())
-  private val doReadWire = Wire(Bool())
+  private val (doIdxCount, doDoneWire) = Counter(doIdxIncWire, oneSPadPSum)
   startIdx := io.ctrlPath.startIdx
   writeOrRead := io.ctrlPath.writeOrRead
-  io.ctrlPath.done := doDone
-  countIncWire := Mux(writeOrRead, doWriteWire, doReadWire)
-  doEnReg := !doDone // FIXME
-  doIdxReg := startIdx + doIdxCount
+  io.ctrlPath.done := doDoneWire
+  doDoneReg := doDoneWire
+  //doEnWire := !doDoneReg && io.ctrlPath.doEn // FIXME
+  doEnWire := io.ctrlPath.doEn // FIXME
+  doIdxWire := startIdx + doIdxCount
   // write logic
-  doWriteWire := io.dataPath.inIOs.valid && doEnReg && writeOrRead
-  when (doWriteWire) {
-    io.dataPath.inIOs.ready := true.B
-    dataSRAM.write(doIdxReg, io.dataPath.inIOs.bits)
-  } .otherwise {
-    io.dataPath.inIOs.ready := false.B
-  }
+  writeLogic(io.dataPath.inIOs, doIdxWire)
   // read logic
-  private val nextValid = Wire(Bool())
-  nextValid := doEnReg && !writeOrRead
-  io.dataPath.outIOs.valid := RegNext(nextValid) // reg next, so one cycle later, data will be read out with valid signal
-  doReadWire := io.dataPath.outIOs.ready && nextValid
-  io.dataPath.outIOs.bits := dataSRAM.read(doIdxReg, doReadWire)
+  readLogic(io.dataPath.outIOs, doIdxWire)
+  // debug io
+  if (debug) {
+    debugLogic(io.debugIO, doIdxWire)
+    io.debugIO.idxCount := doIdxCount
+  } else {
+    io.debugIO <> DontCare
+  }
 }
 
-class InActSRAMCommon(private val theSRAMSize: Int, private val theDataWidth: Int) extends Module {
-  val io = new InACTSRAMCommonIO(theDataWidth)
-  private val theSRAM = SyncReadMem(theSRAMSize, UInt(theDataWidth.W))
+abstract class SRAMCommon(private val theSRAMSize: Int, private val theDataWidth: Int) extends Module {
+  protected val theSRAM: SyncReadMem[UInt] = SyncReadMem(theSRAMSize, UInt(theDataWidth.W))
+  // SRAM read write logic
+  protected val writeOrRead: Bool = Wire(Bool()) // true then "do" means write, false then "do" means read
+  // because it will not read and write at the same time
+  // so we just need one pair of regs to record process
+  protected val doEnWire: Bool = Wire(Bool())
+  protected val doReadWire: Bool = Wire(Bool())
+  protected val doWriteWire: Bool = Wire(Bool())
+  protected val writeInData: UInt = Wire(UInt(theDataWidth.W))
+  protected val readOutData: UInt = Wire(UInt(theDataWidth.W))
+  protected val waitForRead: Bool = RegInit(true.B) // use for letting increase slower when read
+  protected val doIdxIncWire: Bool = Wire(Bool()) // true, then the index increase
+  protected val nextValid: Bool = Wire(Bool())
+  protected val doDoneReg: Bool = RegInit(false.B) // or combinational loop detected
+  doIdxIncWire := Mux(writeOrRead, doWriteWire, doReadWire && waitForRead)
+  def readLogic(readOutIO: DecoupledIO[UInt], doIdx: UInt): Any = {
+    nextValid := doEnWire
+    waitForRead := !waitForRead
+    readOutIO.valid := RegNext(nextValid) // reg next, so one cycle later, data will be read out with valid signal
+    doReadWire := readOutIO.ready && nextValid
+    readOutData := theSRAM.read(doIdx, doReadWire && !waitForRead)
+    readOutIO.bits := Mux(writeOrRead, 0.U, readOutData)
+  }
+
+  def writeLogic(writeInIO: DecoupledIO[UInt], doIdx: UInt): Any = {
+    writeInData := Mux(writeOrRead, writeInIO.bits, 0.U)
+    doWriteWire := doEnWire && writeInIO.valid
+    writeInIO.ready := doWriteWire
+    when (doIdxIncWire) {
+      theSRAM.write(doIdx, writeInData)
+    }
+  }
+
+  def debugLogic(debugIO: SRAMCommonDebugIO, doIdx: UInt): Any = {
+    debugIO.idx := doIdx
+    debugIO.idxInc := doIdxIncWire
+  }
+}
+
+class InActSRAMCommon(private val theSRAMSize: Int, private val theDataWidth: Int, debug: Boolean) extends SRAMCommon(theSRAMSize, theDataWidth) {
+  val io: InACTSRAMCommonIO = IO(new InACTSRAMCommonIO(theSRAMSize, theDataWidth))
   private val doMeetZeroWire = Wire(Bool()) // current data equals to zero
   private val doMightFinishedReg = RegInit(false.B) // that's the end of one SPad's data
   private val writeDoneWire = Wire(Bool())
   private val readDoneWire = Wire(Bool())
+  private val doIdxReg: UInt = RegInit(0.U(log2Ceil(theSRAMSize).W))
   // SRAM read write logic
-  private val writeOrRead = RegInit(false.B) // true then "do" means write, false then "do" means read
   // we assume that one write cycle won't start until read finishes, i.e., the index can be hold when calculating,
   // then continue reading
-  private val doIdxReg: UInt = RegInit(0.U(log2Ceil(theSRAMSize).W))
-  private val doEnReg = RegInit(false.B)
-  private val doReadWire = Wire(Bool())
-  private val writeInData = Wire(UInt(log2Ceil(theSRAMSize).W))
-  private val readOutData = Wire(UInt(log2Ceil(theSRAMSize).W))
   private val currentData = Wire(UInt(log2Ceil(theSRAMSize).W))
-  private val doWriteWire = Wire(Bool())
-  private val doDone: Bool = Wire(Bool()) // meet two continuous zeros
-  private val doIdxIncWire = Wire(Bool()) // true, then the index increase
+  private val doDoneWire = Wire(Bool())
   // TODO: add some logic to check whether one SPad of data is a zero matrix, true then jump
   writeOrRead := io.ctrlPath.writeOrRead
-  io.ctrlPath.done := doDone
-  io.dataPath.outIOs.data.bits := readOutData
-  writeInData := io.dataPath.inIOs.data.bits
-  doEnReg := !doDone // FIXME
-  doIdxIncWire := Mux(writeOrRead, doWriteWire, doReadWire)
+  io.ctrlPath.done := doDoneWire
+  doDoneReg := doDoneWire
+  doEnWire := !doDoneReg && io.ctrlPath.doEn // FIXME
   currentData := Mux(writeOrRead, writeInData, readOutData)
   when (doIdxIncWire) {
     doIdxReg := doIdxReg + 1.U
   }
   // write logic
-  doWriteWire := writeOrRead && doEnReg && io.dataPath.inIOs.data.valid
-  io.dataPath.inIOs.data.ready := doWriteWire
-  when (doWriteWire) {
-    theSRAM.write(doIdxReg, io.dataPath.inIOs.data.bits)
-  }
+  writeLogic(io.dataPath.inIOs.data, doIdxReg)
   // read logic
-  private val nextValid = Wire(Bool())
-  nextValid := doEnReg && !writeOrRead
-  io.dataPath.outIOs.data.valid := RegNext(nextValid) // reg next, so one cycle later, data will be read out with valid signal
-  doReadWire := io.dataPath.outIOs.data.ready && nextValid
-  readOutData := theSRAM.read(doIdxReg, doReadWire)
+  readLogic(io.dataPath.outIOs.data, doIdxReg)
   // do finish?
   doMeetZeroWire := currentData === 0.U
-  doMightFinishedReg := doMeetZeroWire && doEnReg // meets one zero, that's the end of one SPad
+  doMightFinishedReg := doMeetZeroWire && doEnWire // meets one zero, that's the end of one SPad
   // if meet two continuous zeros, then the group of data finishes
-  writeDoneWire := doMeetZeroWire && doEnReg && doMightFinishedReg
+  writeDoneWire := doMeetZeroWire && doEnWire && doMightFinishedReg
   readDoneWire := doMightFinishedReg
-  doDone := Mux(writeOrRead, writeDoneWire, readDoneWire)
+  doDoneWire := Mux(writeOrRead, writeDoneWire, readDoneWire)
+  // debug io
+  if (debug) {
+    debugLogic(io.debugIO, doIdxReg)
+    io.debugIO.idxCount := DontCare
+  } else {
+    io.debugIO <> DontCare
+  }
 }
 
-class InActSRAMBank extends Module with ClusterSRAMConfig {
+class InActSRAMBank(debug: Boolean) extends Module with ClusterSRAMConfig {
   val io = new InActSRAMBankIO
-  private val adrSRAM = Module(new InActSRAMCommon(inActAdrSRAMSize, inActAdrWidth)).io
-  private val dataSRAM = Module(new InActSRAMCommon(inActDataSRAMSize, inActDataWidth)).io
+  private val adrSRAM = Module(new InActSRAMCommon(inActAdrSRAMSize, inActAdrWidth, debug)).io
+  private val dataSRAM = Module(new InActSRAMCommon(inActDataSRAMSize, inActDataWidth, debug)).io
   // SRAM connections
   adrSRAM.dataPath <> io.dataPath.inIOs.adrIOs
   dataSRAM.dataPath <> io.dataPath.inIOs.dataIOs
+  io.ctrlPath.done := adrSRAM.ctrlPath.done && dataSRAM.ctrlPath.done
+  adrSRAM.ctrlPath.writeOrRead := io.ctrlPath.writeOrRead
+  dataSRAM.ctrlPath.writeOrRead := io.ctrlPath.writeOrRead
 }
 
-class InACTSRAMCommonIO(private val dataWidth: Int) extends Bundle {
+class SRAMCommonCtrlIO extends Bundle {
+  val writeOrRead: Bool = Input(Bool())
+  val doEn: Bool = Input(Bool())
+  val done: Bool = Output(Bool())
+}
+
+class SRAMCommonDebugIO(private val theSRAMSize: Int, private val theDataWidth: Int) extends Bundle {
+  val idx: UInt = Output(UInt(log2Ceil(theSRAMSize).W))
+  val idxInc: Bool = Output(Bool())
+  val idxCount: UInt = Output(UInt(log2Ceil(theSRAMSize).W))
+}
+
+trait WithStratIdxIO extends Bundle with ClusterSRAMConfig {
+  val startIdx: UInt = Input(UInt(log2Ceil(pSumSRAMSize).W))
+}
+
+class InACTSRAMCommonIO(private val theSRAMSize: Int, private val theDataWidth: Int) extends Bundle {
   val dataPath = new Bundle {
-    val inIOs = Flipped(new StreamBitsIO(dataWidth))
-    val outIOs = new StreamBitsIO(dataWidth)
+    val inIOs: StreamBitsIO = Flipped(new StreamBitsIO(theDataWidth))
+    val outIOs = new StreamBitsIO(theDataWidth)
   }
-  val ctrlPath = new Bundle {
-    val writeOrRead: Bool = Input(Bool())
-    val done: Bool = Output(Bool())
-  }
+  val ctrlPath = new SRAMCommonCtrlIO
+  val debugIO = new SRAMCommonDebugIO(theSRAMSize, theDataWidth)
 }
 
 class InActSRAMBankIO extends Bundle with ClusterSRAMConfig {
   val dataPath = new StreamBitsInOutIO(inActAdrWidth, inActDataWidth)
+  val ctrlPath = new SRAMCommonCtrlIO
 }
 
 class StreamBitsInOutIO(private val adrWidth: Int, private val dataWidth: Int) extends Bundle {
@@ -141,11 +178,8 @@ class StreamBitsInOutIO(private val adrWidth: Int, private val dataWidth: Int) e
 
 class PSumSRAMBankIO extends Bundle with ClusterSRAMConfig {
   val dataPath = new PSumSRAMDataIO(psDataWidth)
-  val ctrlPath = new Bundle {
-    val startIdx: UInt = Input(UInt(log2Ceil(pSumSRAMSize).W))
-    val writeOrRead: Bool = Input(Bool())
-    val done: Bool = Output(Bool()) // done write or read
-  }
+  val ctrlPath = new SRAMCommonCtrlIO with WithStratIdxIO
+  val debugIO = new SRAMCommonDebugIO(pSumSRAMSize, psDataWidth)
 }
 
 class PSumSRAMDataIO(dataWidth: Int) extends Bundle {
--- src/test/scala/clustertest/ClusterSpecTest.scala
+++ src/test/scala/clustertest/ClusterSpecTest.scala
@@ -3,12 +3,133 @@ package dla.test.clustertest
 import chisel3._
 import chisel3.tester._
 import dla.cluster._
+import dla.pe.MCRENFConfig
 import org.scalatest._
+import scala.util.Random
+import scala.math.pow
 
-class ClusterSpecTest extends FlatSpec with ChiselScalatestTester with Matchers{
+class ClusterSpecTest extends FlatSpec with ChiselScalatestTester with Matchers with ClusterSRAMConfig with MCRENFConfig with GNMFCS2Config {
+  val oneSPadPSum: Int = M0*E*N0*F0 // when read counts this, then stop
+  def InActDataGen(n: Int, dataWidth: Int): List[Int] = {
+    require(n <= pSumSRAMSize/oneSPadPSum, "maybe you should try a smaller 'n'")
+    var resultList: List[Int] = Nil
+    for (i <- 0 until n) {
+      var temResultList: List[Int] = Nil
+      val randomLen: Int = (new Random).nextInt(6) + 3 // the length of one adr SPad range(3, 8)
+      while (temResultList.length < randomLen) {
+        val randomNum = (new Random).nextInt(pow(2, dataWidth).toInt)
+        temResultList = temResultList:::List(randomNum)
+      }
+      temResultList = temResultList:::List(0) // one zero, that's the end of one SPad data
+      resultList = resultList:::temResultList
+    }
+    resultList = resultList:::List(0) // two zeros, that's the end of this stream data
+    resultList
+  }
+  def PSumDataGen(n:Int, dataWidth: Int): List[Int] = {
+    var resultList: List[Int] = Nil
+    while ( resultList.length < n ){
+      val randomNum = (new Random).nextInt(pow(2, dataWidth).toInt)
+      resultList = resultList:::List(randomNum)
+      /*
+      if( !resultList.exists( s=>s==randomNum )){
+        resultList = resultList:::List(randomNum)
+      }
+      */
+    }
+    resultList
+  }
   behavior of "test the spec of cluster group"
-  it should "work well on GLB Cluster" in {}
-  it should "work well on Router Cluster" in {}
-  it should "work well on Processing Element Cluster" in {}
-  it should "work well on Cluster Group" in {}
+  it should "work well on GLB Cluster" in {
+    test(new PSumSRAMBank(pSumSRAMSize, psDataWidth, true)) { thePSumBank =>
+      val theTopIO = thePSumBank.io
+      val theClock = thePSumBank.clock
+      val theData =  PSumDataGen(pSumSRAMSize, psDataWidth)
+      val startIndex = (new Random).nextInt(pSumSRAMSize/oneSPadPSum - 1)*oneSPadPSum
+      println("----------------- test begin -----------------")
+      println("----------- Partial Sum SRAM Bank ------------")
+      println("----------- test basic functions -------------")
+      println(s"-------- startIndex = $startIndex")
+      thePSumBank.reset.poke(true.B)
+      theClock.step(1)
+      thePSumBank.reset.poke(false.B)
+      println("--------------- begin to write ---------------")
+      theTopIO.ctrlPath.writeOrRead.poke(true.B)
+      theTopIO.ctrlPath.doEn.poke(true.B)
+      theTopIO.ctrlPath.startIdx.poke(startIndex.U)
+      for (i <- 0 until oneSPadPSum) {
+        println(s"--------------- $i-th write cycle -----------")
+        theTopIO.dataPath.inIOs.bits.poke(theData(i).U)
+        theTopIO.dataPath.inIOs.valid.poke(true.B)
+        theTopIO.debugIO.idx.expect((startIndex + i).U, s"startIdx = $startIndex")
+        theTopIO.debugIO.idxInc.expect(true.B, s"$i, index should increase")
+        /*
+        println(s"--------       data = ${theData(i)} \n" +
+          s"--------      index = ${theTopIO.debugIO.idx.peek()} \n" +
+          s"-------- whetherInc = ${theTopIO.debugIO.idxInc.peek()}")*/
+        theTopIO.dataPath.inIOs.ready.expect(true.B, s"$i, it should be ready now")
+        theTopIO.ctrlPath.done.expect((i == oneSPadPSum - 1).B, s"i = $i, write should finish?")
+        theClock.step(1)
+        println("-------- PASS")
+      }
+      println("---------------- write finish ----------------")
+      theTopIO.ctrlPath.doEn.poke(false.B)
+      theClock.step(1)
+      println("--------------- begin to read ----------------")
+      theTopIO.ctrlPath.writeOrRead.poke(false.B)
+      theTopIO.ctrlPath.doEn.poke(true.B)
+      theTopIO.ctrlPath.startIdx.poke(startIndex.U)
+      for (i <- 0 until oneSPadPSum) {
+        println(s"--------------- $i-th read cycle -----------")
+        /*
+        println(s"--------       data = ${theTopIO.dataPath.outIOs.bits.peek()} \n" +
+          s"--------      index = ${theTopIO.debugIO.idx.peek()} \n" +
+          s"-------- whetherInc = ${theTopIO.debugIO.idxInc.peek()}")*/
+        theTopIO.dataPath.outIOs.ready.poke(true.B)
+        theTopIO.debugIO.idxInc.expect(false.B, s"$i, index should not increase now")
+        theClock.step(1)
+        /*
+        println(s"--------       data = ${theTopIO.dataPath.outIOs.bits.peek()} \n" +
+          s"--------      index = ${theTopIO.debugIO.idx.peek()} \n" +
+          s"-------- whetherInc = ${theTopIO.debugIO.idxInc.peek()}")*/
+        theTopIO.debugIO.idxInc.expect(true.B, s"index should increase")
+        theTopIO.dataPath.outIOs.bits.expect(theData(i).U, s"$i, theData should be ${theData(i)}")
+        theTopIO.dataPath.outIOs.valid.expect(true.B, "it should valid now")
+        theTopIO.ctrlPath.done.expect((i == oneSPadPSum - 1).B)
+        theClock.step(1)
+        println("-------- PASS")
+      }
+      println("---------------- read finish -----------------")
+      println("---------------- test finish -----------------")
+    }
+    test(new InActSRAMCommon(inActAdrSRAMSize, inActAdrWidth, true)) { theAdrSRAM =>
+      val theTopIO = theAdrSRAM.io
+      val theClock = theAdrSRAM.clock
+      val theData =  InActDataGen(5, inActAdrWidth)
+      println("----------------- test begin -----------------")
+      println("----------- InputActAdr SRAM Bank ------------")
+      println("----------- test basic functions -------------")
+      theAdrSRAM.reset.poke(true.B)
+      theClock.step(1)
+      theAdrSRAM.reset.poke(false.B)
+      println("--------------- begin to write ---------------")
+      theTopIO.ctrlPath.writeOrRead.poke(true.B)
+      theTopIO.ctrlPath.doEn.poke(true.B)
+      for (i <- theData.indices) {
+        println(s"--------------- $i-th write cycle -----------")
+        theTopIO.dataPath.inIOs.data.bits.poke(theData(i).U)
+        theTopIO.dataPath.inIOs.data.valid.poke(true.B)
+        theTopIO.dataPath.inIOs.data.ready.expect(true.B, s"$i, it should be ready now")
+        //theTopIO.ctrlPath.done.expect(((i > 2) && (theData(i-1) == 0) && (theData(i) == 0)).B, s"Data(i-1) = \${theData(i-1)}\n" +
+          s"Data(i) = ${theData(i)}")
+      }
+      println("--------------- write finish -----------------")
+      println("--------------- begin to read ----------------")
+      println("---------------- read finish -----------------")
+      println("---------------- test finish -----------------")
+    }
+  }
+  //it should "work well on Router Cluster" in {}
+  //it should "work well on Processing Element Cluster" in {}
+  //it should "work well on Cluster Group" in {}
 }
