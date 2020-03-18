d70ced2994283689891f2bc440fedd4229fa02b0
SingularityKChen <chency_singularity@163.com>
2020-03-18 12:07:12 +0800

    feat: use regmap instread of RoCC to receive inst
    Use regMap to receive instructions and show CPU the status of
    the dla.

--- dla/src/eyerissTop/EyerissLazy.scala
+++ dla/src/eyerissTop/EyerissLazy.scala
@@ -1,37 +1,47 @@
 package dla.eyerissTop
 import chisel3._
 import chisel3.util._
-/*import freechips.rocketchip.tile._
-import freechips.rocketchip.config._
+import freechips.rocketchip.subsystem.{BaseSubsystem, CacheBlockBytes}
+import freechips.rocketchip.config.{Parameters, Field}
 import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.regmapper.{HasRegMap, RegField}
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util.UIntIsOneOf
 
-class EyerissLazy(opcodes: OpcodeSet, val n: Int = 4)(implicit p: Parameters) extends LazyRoCC(opcodes) {
-  val nXacts = 4      // DMA channel
-  val outFlits = 32   // DMA buffer size
-  val maxBytes = 64   // DMA TileLink max req byte
-  val dmaReader = LazyModule(new StreamReader(nXacts, outFlits, maxBytes)(p))
-  val dmaWriter = LazyModule(new StreamWriter(nXacts, maxBytes)(p))
-  override lazy val module = new EyerissLazyImp(this)
-  tldmaNode :=* dmaReader.node
-  tldmaNode :=* dmaWriter.node
+case class EyerissDecoderParams(address: BigInt, beatBytes: Int) // whether need beatBytes?
 
-}
-
-class EyerissLazyImp(outer: EyerissLazy)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
-
-}*/
-
-class EyerissDecodeModule extends Module {
-  val io: EyerissDecodeIO = IO(new EyerissDecodeIO)
+trait EyerissDecoderModule extends HasRegMap {
+  val io: EyerissDecoderIO
+  implicit val p: Parameters
+  def params: EyerissDecoderParams
+  private val instructionWidth = 32
+  private val compReg = RegInit(false.B) //
+  // if compReg = false, that's idle, then CPU sends one instruction, then compReg becomes true,
+  // one means busy, and the dla will process this instruction.
+  // After this instruction is done, then it becomes false again.
+  // In this case, after configuration and get inAct and weight address, this reg will be assigned
+  // to true, to show it is computing, and when it finishes, it will be false, and wait for read PSum.
+  // When reading PSum, compReg will be assigned to true until it finishes write back.
+  private val instructionReg = RegInit(0.U(instructionWidth.W)) // store instructions from CPU TODO: check bits
+  regmap(
+    0x00 -> Seq(RegField.w(instructionWidth, instructionReg)),
+    0x08 -> Seq(RegField.r(1, compReg))
+  )
   private val func = Wire(UInt(7.W))
   private val rs1 = Wire(UInt(5.W))
   private val rs2 = Wire(UInt(5.W))
   private val rd = Wire(UInt(5.W))
-  private val inActStrAdr = Reg(UInt(5.W))
-  private val weightStrAdr = Reg(UInt(5.W))
-  private val pSumStrAdr = Reg(UInt(5.W))
-  private val gnmfcsRegVec = Seq.fill(12){RegInit(0.U)}
-  private val fnercmRegVec = Seq.fill(6){RegInit(0.U)}
+  private val inActStrAdr = RegInit(0.U(5.W))
+  private val weightStrAdr = RegInit(0.U(5.W))
+  private val pSumStrAdr = RegInit(0.U(5.W))
+  private val gnmfcsRegVec = Seq.fill(12){RegInit(0.U(3.W))}
+  private val fnercmRegVec = Seq.fill(6){RegInit(0.U(3.W))}
+  // TODO: use other instruction format rather than RoCC instruction format
+  func := instructionReg(31, 25)
+  rs2 := instructionReg(34, 20)
+  rs1 := instructionReg(19, 15)
+  rd := instructionReg(11, 7)
+  compReg := Mux(func === 0.U, Mux(io.pSumLoadFin, false.B, true.B), false.B) // TODO: check
   switch(func) {
     is (0.U) { //LoadPSum
       pSumStrAdr := rd
@@ -71,6 +81,10 @@ class EyerissDecodeModule extends Module {
   }
 }
 
-class EyerissDecodeIO extends Bundle {
-  //val req = Decoupled()
+trait EyerissDecoderIO extends Bundle {
+  val pSumLoadFin: Bool = Input(Bool()) // when pSum load finish, then this will be input a true
 }
+
+class EyerissDecoder(c: EyerissDecoderParams)(implicit p: Parameters) extends TLRegisterRouter (
+  c.address, "EyerissDecoder", Seq("SingularityKChen", "EyerissDecoder"), beatBytes = c.beatBytes)(
+  new TLRegBundle(c, _) with EyerissDecoderIO)( new TLRegModule(c, _, _) with EyerissDecoderModule)
