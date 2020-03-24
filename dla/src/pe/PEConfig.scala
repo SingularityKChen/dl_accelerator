0a85074bed7768fc9fec2124bcb64692de3a607d
SingularityKChen <chency_singularity@163.com>
2020-03-24 23:39:21 +0800

    style: add scaladocs,remove unused trait @PEConfig
    + add some scalaDocs;
    + remove the trait NNShapeConfig as only one part use it;

--- dla/src/pe/PEConfig.scala
+++ dla/src/pe/PEConfig.scala
@@ -3,14 +3,22 @@ package dla.pe
 import scala.math.pow
 import chisel3.util.log2Ceil
 
-trait MCRENFConfig extends NNShapeConfig { // contains some scala values
-  protected val M0: Int = 4 // weights reuse M0 times
-  protected val C0: Int = 2 // different input feature maps and their weights reuse
-  protected val R: Int = weightHeight //
-  protected val E: Int = ofmapHeight // same row of weights in a PE
-  protected val N0: Int = 3 // the number of partial sum
-  protected val F0: Int = 1 // one row of partial sum
-  protected val MCRENF: List[Int] = List(M0, C0, R, E, N0, F0)
+trait MCRENFConfig { // contains some scala values
+  /** this contains the parameters needed in SPad level
+    * [[M0]]: weights reuse M0 times
+    * [[C0]]: different input feature maps and their weights reuse
+    * [[R]]:
+    * [[E]]: same row of weights in a PE
+    * [[N0]]: the number of partial sum
+    * [[F0]]: one row of partial sum
+    * [[pSumOneSPadNum]]: the size of one matrix of PSum
+    * */
+  protected val M0: Int = 4
+  protected val C0: Int = 2
+  protected val R: Int = 4
+  protected val E: Int = 2
+  protected val N0: Int = 3
+  protected val F0: Int = 1
   protected val pSumOneSPadNum: Int = M0*E*N0*F0
   // C0*R0 < iacAdrSize = 9
   // C0*R0*E0*N0*F0 <
@@ -29,12 +37,7 @@ trait SPadSizeConfig {
   protected val pSumDataIdxWidth: Int = log2Ceil(pSumDataSPadSize)
 }
 
-trait NNShapeConfig {
-  protected val weightHeight: Int = 4
-  protected val ofmapHeight: Int = 2
-}
-
-trait PESizeConfig extends NNShapeConfig {
+trait PESizeConfig {
   protected val inActDataWidth: Int = 12 // 8-bit data and 4-bit count
   protected val inActAdrWidth: Int = 4
   protected val weightDataWidth: Int = 12 // 24 if SIMD
@@ -44,6 +47,7 @@ trait PESizeConfig extends NNShapeConfig {
   protected val psDataWidth: Int = 20
   protected val fifoSize: Int = 4
   protected val fifoEn: Boolean = true
-  protected val inActZeroColumnCode: Int = pow(2, inActAdrWidth).toInt - 1 // when one address vector's element equals to inActZeroColumnCode, then it is a zero column
+  /** when one address vector's element equals to [[inActZeroColumnCode]], then it is a zero column */
+  protected val inActZeroColumnCode: Int = pow(2, inActAdrWidth).toInt - 1
   protected val weightZeroColumnCode: Int = pow(2, weightAdrWidth).toInt - 1
 }
