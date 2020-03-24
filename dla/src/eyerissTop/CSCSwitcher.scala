f7ee02a640e46e161442544b30a94b901883b168
SingularityKChen <chency_singularity@163.com>
2020-03-24 23:42:09 +0800

    feat: modify the logic of CSCSwitcher

--- dla/src/eyerissTop/CSCSwitcher.scala
+++ dla/src/eyerissTop/CSCSwitcher.scala
@@ -10,43 +10,51 @@ class CSCSwitcherIO(private val adrWidth: Int) extends Bundle with PESizeConfig 
   /** use matrix height and width to increase and wrap csc address and count reg */
   val matrixHeight: UInt = Input(UInt(5.W)) // TODO: check the width
   val matrixWidth: UInt = Input(UInt(5.W))
+  val padNum: UInt = Input(UInt(5.W))
 }
 
 class CSCSwitcher(private val adrWidth: Int) extends Module with PESizeConfig {
   val io: CSCSwitcherIO = IO(new CSCSwitcherIO(adrWidth = adrWidth))
   private val dataWidth = cscDataWidth + cscCountWidth
+  private val zeroCode = if (adrWidth == inActAdrWidth) inActZeroColumnCode else weightZeroColumnCode
   // TODO: generate SIMD csc for weight
   private val inData = Queue(io.inData, fifoSize, flow = true, pipe = true)
   private val outAdr = Wire(Decoupled(UInt(adrWidth.W)))
   private val outData = Wire(Decoupled(UInt(dataWidth.W)))
   private val cscCountReg = RegInit(0.U(cscCountWidth.W))
+  private val cscCountPlusOne = cscCountReg + 1.U
   private val cscAdrReg = RegInit(0.U(adrWidth.W))
+  private val cscAdrPlusOne = cscAdrReg + 1.U
   private val zeroColReg = RegInit(true.B) // true when current column contains zero only
-  private val sramCounter = RegInit(0.U) // counter for one stream
+  private val padNumCounter = RegInit(0.U) // counter for padNumbers
+  private val padNumPlusOne = padNumCounter + 1.U
   private val meetNoneZeroWire = Wire(Bool())
   private val oneColFinWire = Wire(Bool())
   private val oneRowFinWire = Wire(Bool())
-  private val onePadFinRegNext = RegNext(oneColFinWire && oneRowFinWire) // true when process one pad data
+  // TODO: check the logic when inData.fire is false
+  private val onePadFinRegNext = RegNext(inData.fire() && oneColFinWire && oneRowFinWire) // true when process one pad data
+  private val oneStreamFinRegNext = RegNext(inData.fire() && onePadFinRegNext && (padNumPlusOne === io.padNum))
+  /** meetNoneZeroWire will be true when current bits is not zero*/
   meetNoneZeroWire := inData.fire() && inData.bits =/= 0.U
+  private val currentZeroColumn = inData.fire() && oneColFinWire && zeroColReg
+  private val outShouldValid = meetNoneZeroWire || currentZeroColumn
   /** when cscCountReg equals to the height of matrix, then current column finishes */
-  oneColFinWire := io.matrixHeight === cscCountReg
-  oneRowFinWire := io.matrixWidth === cscAdrReg
-  /** when its the last element of one Pad, then ready will be false to stop deq from queue */
-  inData.ready := !onePadFinRegNext
-  /** and both csc data and address will be zero */
-  when (onePadFinRegNext) {
-    outData.bits := 0.U
-    outAdr.bits := 0.U
-  } .otherwise {
-    outData.bits := Cat(inData.bits, cscCountReg) // combine them together
-    outAdr.bits := cscAdrReg
-  }
+  oneColFinWire := io.matrixHeight === cscCountPlusOne
+  oneRowFinWire := io.matrixWidth === cscAdrPlusOne
+  private val currentShouldBeZero = oneStreamFinRegNext || onePadFinRegNext
+  /** when its the last element of one Pad or the whole stream, then ready will be false to stop deq from queue */
+  inData.ready := !currentShouldBeZero
+  /** and both csc data and address will be zero when currentShouldBeZero is true */
+  outAdr.bits := Mux(currentShouldBeZero, 0.U, cscAdrReg)
+  outData.bits := Mux(currentShouldBeZero, 0.U, Mux(currentZeroColumn, zeroCode.U, Cat(inData.bits, cscCountReg)))
+  /** when [[onePadFinRegNext]] equals to true, then pad number should add one */
+  padNumCounter := Mux(onePadFinRegNext, padNumPlusOne, padNumCounter)
   when (oneColFinWire && zeroColReg) {
     /** if current column only contains zero elements */
-    // TODO: should valid for both address and data; should assign data with zeroCode
   }
-  outData.valid := Mux(onePadFinRegNext, true.B, meetNoneZeroWire)
-  outAdr.valid := Mux(onePadFinRegNext, true.B, meetNoneZeroWire && cscAdrReg =/= 0.U)
+  outData.valid := Mux(currentShouldBeZero, true.B, outShouldValid)
+  // TODO: remove `cscAdrReg =/= 0.U` for zero column
+  outAdr.valid := Mux(currentShouldBeZero, true.B, outShouldValid && cscAdrReg =/= 0.U)
   /** when meet none a zero element, zeroColReg will be assigned to false, otherwise keep its value */
   zeroColReg := Mux(meetNoneZeroWire, false.B, zeroColReg)
   when (inData.fire()) {
@@ -54,13 +62,13 @@ class CSCSwitcher(private val adrWidth: Int) extends Module with PESizeConfig {
       cscCountReg := 0.U
       when (oneRowFinWire) {
         cscAdrReg := 0.U
-        zeroColReg := true.B
       } .otherwise {
-        cscAdrReg := 1.U + cscAdrReg
+        cscAdrReg := cscAdrPlusOne
       }
     } .otherwise {
-      cscCountReg := 1.U + cscCountReg
+      cscCountReg := cscCountPlusOne
     }
   }
   io.outData.adrIOs.data <> Queue(outAdr, fifoSize, pipe = true, flow = true)
+  io.outData.dataIOs.data <> Queue(outData, fifoSize, pipe = true, flow = true)
 }
